76748fdca061a90fcd27a02b61c716ac12059039
hong <13671925857@163.com>
2019-10-08 13:27:15 +0800

    1.增加头部view

---
+++ SPSliderDemo/SPAssembleOrderDetail/View/SPAssembleOrderHeaderView.h
@@ -0,0 +1,17 @@
+//
+//  SPAssembleOrderHeaderView.h
+//  SPSliderDemo
+//
+//  Created by hong on 2019/10/8.
+//  Copyright © 2019 hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SPAssembleOrderHeaderView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SPSliderDemo/SPAssembleOrderDetail/View/SPAssembleOrderHeaderView.m
@@ -0,0 +1,30 @@
+//
+//  SPAssembleOrderHeaderView.m
+//  SPSliderDemo
+//
+//  Created by hong on 2019/10/8.
+//  Copyright © 2019 hong. All rights reserved.
+//
+
+#import "SPAssembleOrderHeaderView.h"
+
+@interface SPAssembleOrderHeaderView ()
+
+
+@end
+
+@implementation SPAssembleOrderHeaderView
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+        
+    }
+    return self;
+}
+
+
+
+
+@end
