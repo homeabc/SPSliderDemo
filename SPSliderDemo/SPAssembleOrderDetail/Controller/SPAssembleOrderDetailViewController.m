8390a0a3a4394977c3488bcc9b98d0ca86618b18
hong <13671925857@163.com>
2019-10-08 13:22:17 +0800

    1.提交代码信息

--- SPSliderDemo/SPAssembleOrderDetail/Controller/SPAssembleOrderDetailViewController.m
+++ SPSliderDemo/SPAssembleOrderDetail/Controller/SPAssembleOrderDetailViewController.m
@@ -8,7 +8,12 @@
 
 #import "SPAssembleOrderDetailViewController.h"
 
-@interface SPAssembleOrderDetailViewController ()
+static NSString * const SPAssembleOrderDetailCellIdentifier    = @"SPAssembleOrderDetailCellIdentifier";
+
+
+@interface SPAssembleOrderDetailViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
@@ -21,6 +26,40 @@
  
 }
 
+
+-(UITableView *)tableView
+{
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.showsHorizontalScrollIndicator = NO;
+        _tableView.showsVerticalScrollIndicator = NO;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _tableView.backgroundColor = [UIColor colorWithRed:235/255.0 green:234/255.0 blue:241/255.0 alpha:1];
+        _tableView.estimatedRowHeight = 100;
+        _tableView.estimatedSectionFooterHeight = 0.0f;
+        _tableView.estimatedSectionHeaderHeight = 0.0f;
+        
+    }
+    return _tableView;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SPAssembleOrderDetailCellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:SPAssembleOrderDetailCellIdentifier];
+    }
+    return cell;
+}
 
 
 @end
