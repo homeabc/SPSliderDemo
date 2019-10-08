0ad044c55465d7a7e8766bf6c643b44286a859b3
hong <13671925857@163.com>
2019-10-08 13:13:44 +0800

    1.增加拼团订单详细页面

---
+++ SPSliderDemo/SPAssembleOrderDetail/Controller/SPAssembleOrderDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  SPAssembleOrderDetailViewController.h
+//  SPSliderDemo
+//
+//  Created by hong on 2019/10/8.
+//  Copyright © 2019 hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SPAssembleOrderDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SPSliderDemo/SPAssembleOrderDetail/Controller/SPAssembleOrderDetailViewController.m
@@ -0,0 +1,26 @@
+//
+//  SPAssembleOrderDetailViewController.m
+//  SPSliderDemo
+//
+//  Created by hong on 2019/10/8.
+//  Copyright © 2019 hong. All rights reserved.
+//
+
+#import "SPAssembleOrderDetailViewController.h"
+
+@interface SPAssembleOrderDetailViewController ()
+
+@end
+
+@implementation SPAssembleOrderDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+ 
+}
+
+
+
+@end
